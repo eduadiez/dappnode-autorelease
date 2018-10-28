c72288cbc58f5665383ec4d3cd5bdb45163c3d2d
Eduardo Antuña <eduadiez@gmail.com>
2018-10-28 13:30:18 +0100

    test other way to commig

--- push.sh
+++ push.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
 
 commit_website_files() {
   git checkout -b other
@@ -12,11 +8,8 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin https://${GITHUB_TOKEN}@github.com/eduadiez/dappnode-autorelease.git > /dev/null 2>&1
   git push --quiet --set-upstream origin other
 }
-echo "GITHUB_TOKEN"
-echo $GITHUB_TOKEN
-setup_git
+
 commit_website_files
 upload_files
